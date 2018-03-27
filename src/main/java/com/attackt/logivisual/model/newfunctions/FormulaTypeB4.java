4a912a140abfe72d3bd4551121613aff3893ba0c
inji-hanbin <hanbin@jinji-inc.com>
2018-03-27 15:03:25 +0800

    增加直接引用类型的函数

---
+++ src/main/java/com/attackt/logivisual/model/newfunctions/FormulaTypeB4.java
@@ -0,0 +1,34 @@
+package com.attackt.logivisual.model.newfunctions;
+
+/**
+ * B4类型
+ */
+public enum FormulaTypeB4 {
+    IF(126);
+    // 成员变量
+    private int index;
+    // 构造方法
+    FormulaTypeB4(int index) {
+        this.index = index;
+    }
+    //覆盖方法
+    @Override
+    public String toString() {
+        return this.index+"";
+    }
+    /**
+     * 搜索
+     * @param str
+     * @return 返回值
+     */
+    public static boolean isContainStr(String str)
+    {
+        for (FormulaTypeSearch temp: FormulaTypeSearch.values()) {
+            if(temp.name().equals(str.toUpperCase()))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
